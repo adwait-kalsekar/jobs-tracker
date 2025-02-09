72f1da1a777246081b1859f0c1c4463e4a85b85d
adwait-kalsekar <kalsekaradwait@gmail.com>
2025-02-09 18:12:08 -0500

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/SGp2gSRg7B

---
+++ apps/jobs-auth/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'jobs-auth',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/jobs-auth',
+};
