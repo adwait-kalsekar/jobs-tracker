2aeea3ca29aa6f04e56a73b09099304ba8424724
adwait-kalsekar <kalsekaradwait@gmail.com>
2025-02-09 22:46:31 -0500

    user graphql queries added

--- apps/jobs-auth/src/app/users/users.module.ts
+++ apps/jobs-auth/src/app/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { PrismaModule } from '../prisma-db/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
--- apps/jobs-auth/src/app/users/users.service.ts
+++ apps/jobs-auth/src/app/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma-clients/jobs-auth';
+
+import { PrismaService } from '../prisma-db/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({
+      data: { ...data, password: await hash(data.password, 10) },
+    });
+  }
+
+  async getUsers() {
+    return this.prismaService.user.findMany();
+  }
+}
